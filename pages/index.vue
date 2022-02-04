b26e4068b7c24203dfe12943a78ea9d054021b9a
hung.hoang <hoangdinhhung159@gmail.com>
2022-02-04 13:23:35 +0700

    push

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,56 @@
 <template>
-<v-container>
- <div>
-    <v-btn value="recent">
-        <span>Recent</span>
-  
-        <v-icon>mdi-history</v-icon>
+  <v-container>
+    <div class="top">
+      <v-btn :class="{ 'find-course': true }" value="recent">
+        <v-icon>mdi-magnify</v-icon>
+        <span>Find Course</span>
       </v-btn>
- </div>
-  <!-- <p v-if="role">day la manager</p> -->
-  <v-card
+    </div>
+    <v-card
       v-for="(item, index) in items"
       :key="index"
-      :class='{"course-card": true}'
+      :class="{ 'course-card': true }"
       color="#26c6da"
       dark
       max-width="300"
       :to="'/course/' + item.key"
     >
       <v-card-title>
-        
-        <span class="text-h5 font-weight-bold" >{{ item.title }}</span>
+        <span class="text-h5 font-weight-bold">{{ item.title }}</span>
       </v-card-title>
-  
+
       <v-card-text class="text-h7 font-weight-light">
-        "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+        "Turns out semicolon-less style is easier and safer in TS because most
+        gotcha edge cases are type invalid as well."
       </v-card-text>
-  
+
       <v-card-actions>
         <v-list-item class="grow">
           <v-list-item-avatar color="grey darken-3">
-            <v-icon>
-              mdi-account-outline
-            </v-icon>
+            <v-icon> mdi-account-outline </v-icon>
           </v-list-item-avatar>
-  
+
           <v-list-item-content>
             <v-list-item-title>{{ item.artist }}</v-list-item-title>
           </v-list-item-content>
-  
-          <v-row
-            align="center"
-            justify="end"
-          >
-            <v-icon class="mr-1">
-              mdi-heart
-            </v-icon>
+
+          <v-row align="center" justify="end">
+            <v-icon class="mr-1"> mdi-heart </v-icon>
             <span class="subheading mr-2">256</span>
             <span class="mr-1">·</span>
-            <v-icon class="mr-1">
-              mdi-share-variant
-            </v-icon>
+            <v-icon class="mr-1"> mdi-share-variant </v-icon>
             <span class="subheading">45</span>
           </v-row>
         </v-list-item>
       </v-card-actions>
     </v-card>
-</v-container>
-  
-
-  
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-  middleware: ["isAuthorization"],
+  middleware: ['isAuthorization'],
   data() {
     return {
       items: [
@@ -73,24 +59,33 @@ export default {
           src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
           title: 'Supermodel',
           artist: 'Foster the People',
-          key: "gt1"
+          key: 'gt1',
         },
         {
           color: '#952175',
           src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
           title: 'Halcyon Days',
           artist: 'Ellie Goulding',
-          key: "gt2"
+          key: 'gt2',
         },
       ],
     }
   },
-  
 }
 </script>
 <style scoped>
-  .course-card{
-    display: inline-block;
-    margin: 15px;
-  }
+.course-card {
+  display: inline-block;
+  margin: 15px;
+}
+
+.find-course {
+  position: fixed;
+  right: 15px;
+  top: 76px;
+}
+
+.top{
+  margin: 25px;
+}
 </style>
